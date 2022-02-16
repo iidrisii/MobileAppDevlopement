1166f500533a3bce588f94008bfc5857f1e8a213
Idris <idris2485@gmail.com>
2022-02-16 11:59:02 +0000

    Initial commit

---
+++ App.js
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import LoginScreen from './screens/Login';
+import SignupScreen from './screens/Signup';
+import MainAppScreen from './screens/MainApp';
+
+const Stack = createNativeStackNavigator();
+
+function App() 
+{
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Signup" component={SignupScreen} />
+        <Stack.Screen name="MainApp" component={MainAppScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
